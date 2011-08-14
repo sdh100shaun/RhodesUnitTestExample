f229bdc6818416192e3f2ab43cc943b2f4a33747
Shaun Hare <shaun@shaunhare.co.uk>
2011-08-14 22:21:51 +0100

    getting closer

--- app/test/test_settings.rb
+++ app/test/test_settings.rb
@@ -1,9 +1,15 @@
 $: << "../../app"
-
+$: << "/Library/Ruby/Gems/1.8/gems/rhodes-3.0.2/lib/framework/"
 require 'rubygems'
 gem 'minitest' # ensures you're using the gem, and not the built in MT
 require 'minitest/autorun'
 gem 'rhodes',"3.0.2"
+require 'LocalizationSimplified'
+require 'framework/rhom/rhom'
+require 'framework/rhom'
+require 'framework/rho/rhoapplication'
+require 'framework/rho/rhocontroller'
+require 'framework/rho/render'
 require 'framework/rho/rho'
 
 
